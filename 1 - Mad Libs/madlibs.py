f5c5433605b3693ae1429b13c3a01b23284bf5cd
Anderson Ferreira <anderson97compeng@alu.ufc.br>
2024-03-11 13:07:33 -0300

    Mad Libs game

---
+++ 1 - Mad Libs/madlibs.py
@@ -0,0 +1,11 @@
+# 1 - Mad Libs
+
+adjective = input("Insert an adjective: ")
+verb1 = input("Insert the first verb: ")
+verb2 = input("Insert the second verb: ")
+celebrity = input("Mention a celebrity: ")
+
+madlib = f"Computer programming is so {adjective}! It makes me excited all the time because\
+I love to {verb1}. Stay hydrated and {verb2}, like you are {celebrity}!"
+
+print("\n" + madlib)
